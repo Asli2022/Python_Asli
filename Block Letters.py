2cd03778a146ae0138695e979e00c784ce5f4b16
unknown <ashleyaden4u@hotmail.com>
2022-08-09 13:05:32 +0100

    Initial commit

---
+++ Block Letters.py
@@ -0,0 +1,13 @@
+
+name = """
+
+          A                sss              L            IIIIIII
+        A    A           s      s           L               I
+       A       A         s                  L               I
+       AAAAAAAAAA         s                 L               I
+      A         A           sss             L               I
+      A          A                 s        L               I
+      A          A        s        s        L               I
+      A          A           sss            L L L L L L   IIIIIII
+"""
+print(name)
